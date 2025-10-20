170e1400eb32a1bbd3a3c1ea7c0a0b78648b794c
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-10-20 15:36:38 -0300

    closes #3 - mensagem de erro

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 
+
 function search() {
+    $("#error").html(" ");
+
     var userName = document.getElementById("inputUser").value;
 
-    var userAPI = ` https://api.github.com/users/${userName}`;
+    var userAPI = `https://api.github.com/users/${userName}`;
 
     $.getJSON(userAPI, (user) => {
 
-        document.getElementById("userData").innerHTML =
-            `<div class="container bg-light bg-gradient rounded shadow p-3">
+        showUser(user);
+
+    }).fail(() => {
+
+        showMessageError("Usuário não encontrado! Tente novamente.");
+
+        cleanError();
+    });
+};
+
+function showUser(user) {
+
+    $("#userData").html(
+        `<div class="container bg-light bg-gradient rounded shadow p-3">
                 <div class="mt-2" id="name">
                     ${user.name}
                 </div>
@@ -17,12 +32,20 @@ function search() {
                 </div>
 
                 <div id="company">
-                    ${user.company}
+                    ${user.company ? user.company : "Empresa não informada"}
                 </div>
 
                 <img class="rounded shadow mt-2" width="200" height="200"
-                    src=${user.avatar_url} id="avatar_url">
+                    src=${user.avatar_url} id="avatar_url" alt="avatarUser">
             </div>`
-    }
     );
 };
+
+
+function showMessageError(msg) {
+    $("#error").html(`<div class='alert alert-danger mt-2' role='alert'>${msg}</div>`);
+};
+
+function cleanError() {
+    $("#userData").html(" ");
+}
